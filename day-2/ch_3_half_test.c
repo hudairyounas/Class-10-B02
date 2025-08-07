d5468ad9b3446c8b8ce1d083fdb0cbe2091e9ddd
Hudair Younis <hudairshorts@gmail.com>
2025-08-07 12:23:35 +0500

    ch 3 first half test

---
+++ day-2/ch_3_half_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+    //! 1. Define conditional logic?
+
+    //! 2. What is Selection Statement?
+
+    //! 3. Define if statement and also write its structure.
+
+    //! 4. Write a program to check if the number is even?
+
+    //! 5. What is Sequential Control Statement?
+
+    //! 6. write a program that takes 2 number as input and check if the 1st number is grater than 2nd number than it print 1st otherwise it print 2nd one.
+
+    //! 7. A marketing firm calculates the salary of its employees according to the following formula.
+    //! Gross Salary = Basic Salary + (Number of Items Sold X 8) + Bonus
+    //! If the number of sold items are more than 100 and the number of broken items are 0, then bonus is Rs. 10000, otherwise bonus is 0.
+    //! Write a program that takes basic salary, the number of sold and broken  items as input from user, then calculates and displays the gross salary of the employee.
+
+    //! 8. Write a program that takes the age of a person as an input and displays "Teenager" if the age lies between 13 and 19.
+}

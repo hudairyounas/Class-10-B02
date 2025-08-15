15de2bc6ba8b7ded3ab676780c7a65b6deaba89e
Hudair Younis <hudairshorts@gmail.com>
2025-08-15 22:18:35 +0500

    add an interview question

--- day-3/main.c
+++ day-3/main.c
@@ -16,4 +16,10 @@ void main()
     {
         printf("Number is Negative");
     }
+
+    //! Tesla interview Question
+
+    //! 100 prisoners. they all are in 1 direction. every prisoners have wear only blue or red hat and every prisoner have only see the next person hat color not its and and the person wo stand on the behalf of it. now the officer went their and check all the hat color and if the prisoners tell the right answer that what its hat color than the prisoner will go otherwise the prisoner will kill them.
+
+    //! so the question is which strategy that all the prisoners that make in which maximum number of prisoners will safe.
 }

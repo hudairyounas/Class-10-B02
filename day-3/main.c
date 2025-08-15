b2ec01df2c61aa83943ffa7fea31cda36a9b65e0
Hudair Younis <hudairshorts@gmail.com>
2025-08-15 21:33:01 +0500

    day 3 code

---
+++ day-3/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+    int num = 10;
+
+    if (num > 0)
+    {
+        printf("Number is Positive");
+    }
+    else if (num < 0)
+    {
+        printf("Number is Negative");
+    }
+    else
+    {
+        printf("Number is Negative");
+    }
+}
